8e060881dcd401822bb14a89585f17eb7a17bbc9
Lennart <lennart.hohmann@med.lu.se>
2023-01-18 13:02:19 +0100

    updated clin var table creation

--- 1_clinical/clin_variables.R
+++ 1_clinical/clin_variables.R
@@ -64,7 +64,7 @@ if (cohort=="Metabric") {
     anno$HER2_Low[is.na(anno$HER2_Low)] <- 0
     
     # 
-    anno$IC10 <- as.factor(anno$IC10)
+    anno$IC10 <- factor(anno$IC10, levels=c("1","2","3","4ER+","6","7","8","9","10"))
     
 # for SCANB cohort
 } else if (cohort=="SCANB") {
@@ -177,22 +177,6 @@ size.df <- test.cont(data = size.data,
 size.df
 
 #######################################################################
-# 4. PR status (2x2 contingency table)
-#######################################################################
-
-pr.data <- anno %>% filter(!is.na(PR))
-
-# matrix
-pr.df <- as.data.frame(matrix(ncol=length(col.names),nrow = 2))
-names(pr.df) <- col.names
-pr.df$Variable <- c("PR.neg","PR.pos")
-
-pr.df <- test.2x2ct(data = pr.data,
-                    var.df = pr.df,
-                    var = "PR")
-pr.df
-
-#######################################################################
 # 4. NHG (2x3 contingency table)
 #######################################################################
 
@@ -230,179 +214,247 @@ ln.df <- test.2x2ct(data = ln.data,
 # 4. IC10 (only Metabric)
 if (cohort=="Metabric") {
 #######################################################################
-  # this
-}
-
-if (cohort=="SCANB") {
-  # review and table exports
-}
-########################################################################
-# 2. review data vars
-
-#######################################################################
-# % reviewed 
-#######################################################################
-
-# matrix
-rev.count <- as.data.frame(matrix(ncol=length(col.names)))
-names(rev.count) <- col.names
-rev.count$Variable <- "N.Reviewed"
-
-t.rev <- table(anno[which(anno$Bosch_RS1==1),]$PAM50)
-t.all <- table(anno$PAM50)
-
-# add count data
-rev.count$`HER2E(ref)`[1] <- t.rev[1]
-rev.count$LUMA[1] <- t.rev[2]
-rev.count$LUMB[1] <- t.rev[3]
-
-# add % data
-rev.count$`HER2E.%`[1] <- (t.rev[1]/t.all[1])*100
-rev.count$`LUMA.%`[1] <- (t.rev[2]/t.all[2])*100
-rev.count$`LUMB.%`[1] <- (t.rev[3]/t.all[3])*100
-
-rev.count
-
-#######################################################################
-# filter anno to only include review data
-anno <- anno %>% filter(Bosch_RS1 == 1)
-
-#######################################################################
-# HER2low frequency (2x2 contingency table)
-#######################################################################
-
-h2low.data <- anno %>% filter(!is.na(HER2_Low))
-table(anno$PAM50,anno$HER2_Low)
-
-# matrix
-h2low.df <- as.data.frame(matrix(ncol=length(col.names),nrow = 2))
-names(h2low.df) <- col.names
-h2low.df$Variable <- c("HER2low.No","HER2low.Yes")
-
-h2low.df <- test.2x2ct(data = h2low.data,
-                       var.df = h2low.df,
-                       var = "HER2_Low")
-
-
-h2low.df
-
-#######################################################################
-# PAM50 count in review
-#######################################################################
-
-# matrix
-pam50.count.rev <- as.data.frame(matrix(ncol=length(col.names)))
-names(pam50.count.rev) <- col.names
-pam50.count.rev$Variable <- "N.Bosch"
-
-t <- table(anno$PAM50)
-
-# add count data
-pam50.count.rev$`HER2E(ref)`[1] <- t[1]
-pam50.count.rev$LUMA[1] <- t[2]
-pam50.count.rev$LUMB[1] <- t[3]
-
-# add % data
-pam50.count.rev$`HER2E.%`[1] <- (t[1]/length(anno$PAM50))*100
-pam50.count.rev$`LUMA.%`[1] <- (t[2]/length(anno$PAM50))*100
-pam50.count.rev$`LUMB.%`[1] <- (t[3]/length(anno$PAM50))*100
-
-pam50.count.rev
-
-#######################################################################
-# LN Bosch
-#######################################################################
-
-ln_bosch.data <- anno %>% filter(!is.na(LN_Bosch))
-table(anno$PAM50,anno$LN_Bosch)
-
-# matrix
-ln_bosch.df <- as.data.frame(matrix(ncol=length(col.names),nrow = 2))
-names(ln_bosch.df) <- col.names
-ln_bosch.df$Variable <- c("LN.N0.Bosch","LN.N+.Bosch")
-
-ln_bosch.df <- test.2x2ct(data = ln_bosch.data,
-                          var.df = ln_bosch.df,
-                          var = "LN_Bosch")
-
-ln_bosch.df
-
-#######################################################################
-# Size Bosch x.data .df VAR
-#######################################################################
-
-size_bosch.data <- anno %>% filter(!is.na(Size_Bosch))
-table(anno$PAM50,is.na(anno$Size_Bosch))
-
-# matrix
-size_bosch.df <- as.data.frame(matrix(ncol=length(col.names)))
-names(size_bosch.df) <- col.names
-size_bosch.df$Variable <- "Size.Bosch"
-
-size_bosch.df <- test.cont(data = size_bosch.data,
-                           var.df = size_bosch.df,
-                           var = "Size_Bosch")
-
-size_bosch.df
-
-#######################################################################
-# NHG Bosch
-#######################################################################
-
-nhg_bosch.data <- anno %>% filter(!is.na(NHG_Bosch))
-table(anno$PAM50,is.na(anno$NHG_Bosch))
-
-# matrix
-nhg_bosch.df <- as.data.frame(matrix(ncol=length(col.names),nrow = 3))
-names(nhg_bosch.df) <- col.names
-nhg_bosch.df$Variable <- c("NHG1.Bosch","NHG2.Bosch","NHG3.Bosch")
-
-nhg_bosch.df <-   test.2x3ct(data = nhg_bosch.data,
-                       var.df = nhg_bosch.df,
-                       var = "NHG_Bosch")
-
-nhg_bosch.df
-
-#######################################################################
-# Age for review
-#######################################################################
-
-age_bosch.data <- anno %>% filter(!is.na(Age))
-table(anno$PAM50,is.na(anno$Age))
-
-# matrix
-age_bosch.df <- as.data.frame(matrix(ncol=length(col.names)))
-names(age_bosch.df) <- col.names
-age_bosch.df$Variable <- "Age"
-
-age_bosch.df <- test.cont(data = age_bosch.data,
-                    var.df = age_bosch.df,
-                    var = "Age")
-
-age_bosch.df
-
-#######################################################################
-# PR for review
-#######################################################################
-
-pr_bosch.data <- anno %>% filter(!is.na(PR))
-
-# matrix
-pr_bosch.df <- as.data.frame(matrix(ncol=length(col.names),nrow = 2))
-names(pr_bosch.df) <- col.names
-pr_bosch.df$Variable <- c("PR.neg","PR.pos")
-
-pr_bosch.df <- test.2x2ct(data = pr_bosch.data,
-                    var.df = pr_bosch.df,
-                    var = "PR")
-pr_bosch.df
-
-#######################################################################
-# export to excel
-#######################################################################
-
-if (cohort == "SCANB") {
   
+  var <- "IC10"
+  data <- anno %>% filter(!is.na(IC10))
+  table(anno$PAM50,is.na(anno$IC10))
+  
+  # matrix
+  var.df <- as.data.frame(matrix(ncol=length(col.names),nrow = 9))
+  names(var.df) <- col.names
+  var.df$Variable <- c("IC-1","IC-2","IC-3","IC-4ER+","IC-6","IC-7","IC-8","IC-9","IC-10") # no ic5 classification, add later in table
+  
+  # for Her2 vs LumA
+  luma.ct <- table(data$PAM50,data[[var]])[c("Her2","LumA"),]
+    
+  # for Her2 vs LumB
+  lumb.ct <- table(data$PAM50,data[[var]])[c("Her2","LumB"),]
+  
+  groups <- c("Her2","LumA","LumB")
+  cols <- c("HER2E","LUMA","LUMB")
+  
+  for (i in 1:3) {
+    type <- groups[i]
+    col <- cols[i]
+    type.dat <- data[which(data$PAM50==type),]
+    type.dat$PAM50 <- droplevels(type.dat$PAM50)
+    type.counts <- table(type.dat$PAM50,type.dat[[var]])
+    
+    # count column
+    for (j in 1:length(unique(data$IC10))) {
+      if (col=="HER2E") {
+        var.df[[paste(col,"(ref)",sep="")]][j] <- type.counts[j]
+      } else {
+        var.df[[col]][j] <- type.counts[j]
+      }
+    
+      # % column
+      var.df[[paste(col,".%",sep="")]][j] <- round(type.counts[j]/sum(type.counts)*100)
+      var.df[[paste(col,".%",sep="")]][j] <- round(type.counts[j]/sum(type.counts)*100)
+      var.df[[paste(col,".%",sep="")]][j] <- round(type.counts[j]/sum(type.counts)*100)
+    }
+  }
+  
+  ic10.df <- var.df
+  
+  # create grouped barplot
+  dat <- as.data.frame(table(anno$PAM50,anno$IC10)) %>% 
+    mutate(Freq=case_when(Var1=="Her2" ~ (Freq/table(anno$PAM50)[["Her2"]])*100,
+                          Var1=="LumA" ~ (Freq/table(anno$PAM50)[["LumA"]])*100,
+                          Var1=="LumB" ~ (Freq/table(anno$PAM50)[["LumB"]])*100))
+  # Create grouped barplot using ggplot2
+  ggplot(dat,aes(x = Var2, y =Freq, fill = Var1)) +
+    geom_bar(stat = "identity", position = "dodge") +
+    scale_fill_manual(name = "PAM50 Subtype",values=c("#d334eb", "#34c6eb", "#2176d5")) +
+    xlab("IC10 Subtype") +
+    ylab("Fraction (%)")
+}
+
+#######################################################################
+# only for SCANB
+if (cohort=="SCANB") {
+  #######################################################################
+  # 4. PR status (2x2 contingency table)
+  #######################################################################
+  
+  pr.data <- anno %>% filter(!is.na(PR))
+  
+  # matrix
+  pr.df <- as.data.frame(matrix(ncol=length(col.names),nrow = 2))
+  names(pr.df) <- col.names
+  pr.df$Variable <- c("PR.neg","PR.pos")
+  
+  pr.df <- test.2x2ct(data = pr.data,
+                      var.df = pr.df,
+                      var = "PR")
+  pr.df
+  
+  
+  ########################################################################
+  # 2. review data vars
+  
+  #######################################################################
+  # % reviewed 
+  #######################################################################
+  
+  # matrix
+  rev.count <- as.data.frame(matrix(ncol=length(col.names)))
+  names(rev.count) <- col.names
+  rev.count$Variable <- "N.Reviewed"
+  
+  t.rev <- table(anno[which(anno$Bosch_RS1==1),]$PAM50)
+  t.all <- table(anno$PAM50)
+  
+  # add count data
+  rev.count$`HER2E(ref)`[1] <- t.rev[1]
+  rev.count$LUMA[1] <- t.rev[2]
+  rev.count$LUMB[1] <- t.rev[3]
+  
+  # add % data
+  rev.count$`HER2E.%`[1] <- (t.rev[1]/t.all[1])*100
+  rev.count$`LUMA.%`[1] <- (t.rev[2]/t.all[2])*100
+  rev.count$`LUMB.%`[1] <- (t.rev[3]/t.all[3])*100
+  
+  rev.count
+  
+  #######################################################################
+  # filter anno to only include review data
+  anno <- anno %>% filter(Bosch_RS1 == 1)
+  
+  #######################################################################
+  # HER2low frequency (2x2 contingency table)
+  #######################################################################
+  
+  h2low.data <- anno %>% filter(!is.na(HER2_Low))
+  table(anno$PAM50,anno$HER2_Low)
+  
+  # matrix
+  h2low.df <- as.data.frame(matrix(ncol=length(col.names),nrow = 2))
+  names(h2low.df) <- col.names
+  h2low.df$Variable <- c("HER2low.No","HER2low.Yes")
+  
+  h2low.df <- test.2x2ct(data = h2low.data,
+                         var.df = h2low.df,
+                         var = "HER2_Low")
+  
+  
+  h2low.df
+  
+  #######################################################################
+  # PAM50 count in review
+  #######################################################################
+  
+  # matrix
+  pam50.count.rev <- as.data.frame(matrix(ncol=length(col.names)))
+  names(pam50.count.rev) <- col.names
+  pam50.count.rev$Variable <- "N.Bosch"
+  
+  t <- table(anno$PAM50)
+  
+  # add count data
+  pam50.count.rev$`HER2E(ref)`[1] <- t[1]
+  pam50.count.rev$LUMA[1] <- t[2]
+  pam50.count.rev$LUMB[1] <- t[3]
+  
+  # add % data
+  pam50.count.rev$`HER2E.%`[1] <- (t[1]/length(anno$PAM50))*100
+  pam50.count.rev$`LUMA.%`[1] <- (t[2]/length(anno$PAM50))*100
+  pam50.count.rev$`LUMB.%`[1] <- (t[3]/length(anno$PAM50))*100
+  
+  pam50.count.rev
+  
+  #######################################################################
+  # LN Bosch
+  #######################################################################
+  
+  ln_bosch.data <- anno %>% filter(!is.na(LN_Bosch))
+  table(anno$PAM50,anno$LN_Bosch)
+  
+  # matrix
+  ln_bosch.df <- as.data.frame(matrix(ncol=length(col.names),nrow = 2))
+  names(ln_bosch.df) <- col.names
+  ln_bosch.df$Variable <- c("LN.N0.Bosch","LN.N+.Bosch")
+  
+  ln_bosch.df <- test.2x2ct(data = ln_bosch.data,
+                            var.df = ln_bosch.df,
+                            var = "LN_Bosch")
+  
+  ln_bosch.df
+  
+  #######################################################################
+  # Size Bosch x.data .df VAR
+  #######################################################################
+  
+  size_bosch.data <- anno %>% filter(!is.na(Size_Bosch))
+  table(anno$PAM50,is.na(anno$Size_Bosch))
+  
+  # matrix
+  size_bosch.df <- as.data.frame(matrix(ncol=length(col.names)))
+  names(size_bosch.df) <- col.names
+  size_bosch.df$Variable <- "Size.Bosch"
+  
+  size_bosch.df <- test.cont(data = size_bosch.data,
+                             var.df = size_bosch.df,
+                             var = "Size_Bosch")
+  
+  size_bosch.df
+  
+  #######################################################################
+  # NHG Bosch
+  #######################################################################
+  
+  nhg_bosch.data <- anno %>% filter(!is.na(NHG_Bosch))
+  table(anno$PAM50,is.na(anno$NHG_Bosch))
+  
+  # matrix
+  nhg_bosch.df <- as.data.frame(matrix(ncol=length(col.names),nrow = 3))
+  names(nhg_bosch.df) <- col.names
+  nhg_bosch.df$Variable <- c("NHG1.Bosch","NHG2.Bosch","NHG3.Bosch")
+  
+  nhg_bosch.df <-   test.2x3ct(data = nhg_bosch.data,
+                         var.df = nhg_bosch.df,
+                         var = "NHG_Bosch")
+  
+  nhg_bosch.df
+  
+  #######################################################################
+  # Age for review
+  #######################################################################
+  
+  age_bosch.data <- anno %>% filter(!is.na(Age))
+  table(anno$PAM50,is.na(anno$Age))
+  
+  # matrix
+  age_bosch.df <- as.data.frame(matrix(ncol=length(col.names)))
+  names(age_bosch.df) <- col.names
+  age_bosch.df$Variable <- "Age"
+  
+  age_bosch.df <- test.cont(data = age_bosch.data,
+                      var.df = age_bosch.df,
+                      var = "Age")
+  
+  age_bosch.df
+  
+  #######################################################################
+  # PR for review
+  #######################################################################
+  
+  pr_bosch.data <- anno %>% filter(!is.na(PR))
+  
+  # matrix
+  pr_bosch.df <- as.data.frame(matrix(ncol=length(col.names),nrow = 2))
+  names(pr_bosch.df) <- col.names
+  pr_bosch.df$Variable <- c("PR.neg","PR.pos")
+  
+  pr_bosch.df <- test.2x2ct(data = pr_bosch.data,
+                      var.df = pr_bosch.df,
+                      var = "PR")
+  pr_bosch.df
+  
+  #######################################################################
+  # export to excel
+  #######################################################################
+    
   # rel4+part of rs1
   sheet.list.table1 <- list("N" = pam50.count, 
                            "Age" = age.df,
@@ -436,19 +488,14 @@ if (cohort == "SCANB") {
   # 1 table
   sheet.list.table1 <- list("N" = pam50.count, 
                             "Age" = age.df,
-                            "PR" = pr.df,
                             "Size" = size.df,
                             "NHG" = nhg.df,
                             "LN" = ln.df,
-                            "HER2low" = h2low.df)
-  # IC10
+                            "IC10" = ic10.df) 
   
   # rbind
   table1 <- as.data.frame(do.call("rbind", sheet.list.table1)) %>% mutate_at(vars(colnames(.)[colnames(.) %!in% c("Variable","LUMA.pval","LUMB.pval")]), function(x) {round(x,1)})
   
   write_xlsx(table1,"./output/supplementary_data/Metabric_clinpath_table1.xlsx")
 }
-
-# save
-#write.xlsx(final_matrix, file = paste(data.path,"clin_variables.xlsx",sep=""),overwrite = TRUE)
 
