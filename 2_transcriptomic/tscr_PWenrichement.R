b12d134463bfca5f639a6ffdbd20c9a5eb0395cd
Lennart <lennart.hohmann@med.lu.se>
2023-03-07 11:29:10 +0100

    added more terms to pwe

--- 2_transcriptomic/tscr_PWenrichement.R
+++ 2_transcriptomic/tscr_PWenrichement.R
@@ -39,41 +39,40 @@ DE.res.metabric <- loadRData("./data/METABRIC/2_transcriptomic/processed/DE_resu
 # set database parameters
 setEnrichrSite("Enrichr") 
 dbs <- listEnrichrDbs() # see avaiable dbs and select 
-dbs <- c("KEGG_2021_Human") #"GO_Molecular_Function_2021", "GO_Biological_Process_2021
+dbs <- c("GO_Biological_Process_2021") #"KEGG_2021_Human", "GO_Molecular_Function_2021", "GO_Biological_Process_2021
 
 #######################################################################
-# Top DEGs
+# All DEGs
 #######################################################################
+
+#filter based on expression diff?
+#like   filter(abs(Her2.LumA.diff) >= 1) %>% 
 
 # define DEG set
-top.DEGs.scanb <- DE.res.scanb %>% 
+DEGs.scanb <- DE.res.scanb %>% 
   filter(Her2.LumA.padj <= 0.05) %>% 
   filter(Her2.LumB.padj <= 0.05) %>% 
-  filter(abs(Her2.LumA.diff) >= 1) %>% 
-  filter(abs(Her2.LumB.diff) >= 1) %>% 
   rownames_to_column("Gene") %>% pull(Gene)
 
-top.DEGs.metabric <- DE.res.metabric %>% 
+DEGs.metabric <- DE.res.metabric %>% 
   filter(Her2.LumA.padj <= 0.05) %>% 
   filter(Her2.LumB.padj <= 0.05) %>% 
-  filter(abs(Her2.LumA.diff) >= 1) %>% 
-  filter(abs(Her2.LumB.diff) >= 1) %>% 
   rownames_to_column("Gene") %>% pull(Gene) 
 
-# core top gex
-top.DEGs.core <- intersect(top.DEGs.scanb,top.DEGs.metabric)
+# core gex
+DEGs.core <- intersect(DEGs.scanb,DEGs.metabric)
 
 #---------------------------------------------------------------------#
 
 #assign("res", enrichr(top.DEGs.core, dbs)) 
 
-res <- as.data.frame(enrichr(top.DEGs.core, dbs)[[1]])
+res <- as.data.frame(enrichr(DEGs.core, dbs)[[1]])
 
 plot.list <- append(plot.list, list(
-  plotEnrich(res, showTerms = 5, numChar = 60, title = "Top core DEGs") +
+  plotEnrich(res, showTerms = 10, numChar = 60, y = "Count", orderBy = "P.value", title = paste("Core DEGs (n = ",length(DEGs.core),")",sep="")) +
     theme(text = element_text(size = 20))))
 
-xlsx.list <- append(xlsx.list, list("Top_coreDEGs" = res))
+xlsx.list <- append(xlsx.list, list("CoreDEGs" = res))
 
 #######################################################################
 # HER2E vs. LumA
@@ -82,17 +81,15 @@ xlsx.list <- append(xlsx.list, list("Top_coreDEGs" = res))
 # define DEG set
 LumA.DEGs.scanb <- DE.res.scanb %>% 
   filter(Her2.LumA.padj <= 0.05) %>% 
-  filter(abs(Her2.LumA.diff) >= 1) %>% 
   rownames_to_column("Gene") %>% 
   pull(Gene)
 
 LumA.DEGs.metabric <- DE.res.metabric %>% 
   filter(Her2.LumA.padj <= 0.05) %>% 
-  filter(abs(Her2.LumA.diff) >= 1) %>% 
   rownames_to_column("Gene") %>% 
   pull(Gene) 
 
-# core top gex
+# gex
 LumA.DEGs.core <- intersect(LumA.DEGs.scanb,LumA.DEGs.metabric)
 
 #---------------------------------------------------------------------#
@@ -100,7 +97,7 @@ LumA.DEGs.core <- intersect(LumA.DEGs.scanb,LumA.DEGs.metabric)
 res <- as.data.frame(enrichr(LumA.DEGs.core, dbs)[[1]])
 
 plot.list <- append(plot.list, list(
-  plotEnrich(res, showTerms = 5, numChar = 60, y = "Count", orderBy = "P.value", title = "LumA core DEGs") +
+  plotEnrich(res, showTerms = 10, numChar = 60, y = "Count", orderBy = "P.value", title = paste("LumA core DEGs (n = ",length(LumA.DEGs.core),")",sep="")) +
     theme(text = element_text(size = 20))))
 
 xlsx.list <- append(xlsx.list, list("LumA_coreDEGs" = res))
@@ -112,13 +109,11 @@ xlsx.list <- append(xlsx.list, list("LumA_coreDEGs" = res))
 # define DEG set
 LumB.DEGs.scanb <- DE.res.scanb %>% 
   filter(Her2.LumB.padj <= 0.05) %>% 
-  filter(abs(Her2.LumB.diff) >= 1) %>% 
   rownames_to_column("Gene") %>% 
   pull(Gene)
 
 LumB.DEGs.metabric <- DE.res.metabric %>% 
   filter(Her2.LumB.padj <= 0.05) %>% 
-  filter(abs(Her2.LumB.diff) >= 1) %>% 
   rownames_to_column("Gene") %>% 
   pull(Gene) 
 
@@ -130,7 +125,7 @@ LumB.DEGs.core <- intersect(LumB.DEGs.scanb,LumB.DEGs.metabric)
 res <- as.data.frame(enrichr(LumB.DEGs.core, dbs)[[1]])
 
 plot.list <- append(plot.list, list(
-  plotEnrich(res, showTerms = 5, numChar = 60, y = "Count", orderBy = "P.value", title = "LumB core DEGs") +
+  plotEnrich(res, showTerms = 10, numChar = 60, y = "Count", orderBy = "P.value", title = paste("LumB core DEGs (n = ",length(LumB.DEGs.core),")",sep="")) +
     theme(text = element_text(size = 20))))
 
 xlsx.list <- append(xlsx.list, list("LumB_coreDEGs" = res))
