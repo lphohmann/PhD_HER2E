071b618cedf7137c5022e9a07dd4c12b5a8ac68a
Lennart <l.p.hohmann@gmail.com>
2023-09-27 14:39:45 +0200

    fixed error in getting mut counts

--- 3_genomic/gen_driver_barplots.R
+++ 3_genomic/gen_driver_barplots.R
@@ -65,17 +65,15 @@ pindel.files <- list.files(
   pattern="*.annot.vcf.gz$", full.names = TRUE, recursive = TRUE)
 
 # load the files
-#caveman.files <- lapply(caveman.files,read.table)  
 caveman.files <- lapply(caveman.files, function(x) {
   # load
   file <- read.vcfR(x)
   sample <- gsub(".*caveman/(.+)_vs.*", "\\1", x)
   file.dat <- as.data.frame(file@fix) %>% 
     # make CLPM and ASMD into own column
-    mutate(ASMD = as.numeric(gsub(".*ASMD=(.+);VT.*", "\\1", INFO))) %>% 
-    mutate(CLPM = as.numeric(gsub(".*CLPM=(.+);ASMD.*", "\\1", INFO))) %>% 
-    filter(CLPM==0 & ASMD >=140) #Caveman counts (CLPM=0,ASMD>=140)_final
-  
+    mutate(ASMD = as.numeric(gsub(".*ASMD=([0-9.]+);.*", "\\1", INFO))) %>% 
+    mutate(CLPM = as.numeric(gsub(".*CLPM=([0-9.]+);.*", "\\1", INFO))) %>% 
+    filter(FILTER=="PASS" & CLPM==0 & ASMD >=140) #Caveman counts (CLPM=0,ASMD>=140)_final
   return(c(sample, nrow(file.dat)))
 })  
 cv <- as.data.frame(do.call(rbind,caveman.files))
@@ -87,54 +85,37 @@ pindel.files <- lapply(pindel.files, function(x) {
   sample <- gsub(".*pindel/(.+)_vs.*", "\\1", x)
   file.dat <- as.data.frame(file@fix) %>% 
     # make repeats column
-    mutate(Repeats = as.numeric(gsub(".*REP=(.+);VT.*", "\\1", INFO))) %>% 
-    filter(QUAL>=250 & Repeats>10) #Pindel counts (QUAL>=250,Repeats>10)_final
-  
+    mutate(Repeats = as.numeric(gsub(".*REP=([0-9]+);.*", "\\1", INFO))) %>% 
+    filter(FILTER=="PASS" & QUAL>=250 & Repeats<10) #Pindel counts (QUAL>=250,Repeats<10)_final
   return(c(sample, nrow(file.dat)))
 })
+
 pd <- as.data.frame(do.call(rbind,pindel.files))
 names(pd) <- c("sample","pindel_count")
 
 # merge and correct IDs
 res <- merge(cv,pd,by="sample")
 save(res, file="./data/SCANB/3_genomic/processed/mutation_counts.RData")
-
+res <- loadRData("./data/SCANB/3_genomic/processed/mutation_counts.RData")
 
 # correct ids now
-
-
-
-
-
-
-# get down to one file per sample
-c(caveman.files, pindel.files)
-
-# convert the file names to S identifiers
-# sample ids used in the ascat files
-ascat.ids <- as.data.frame(unlist(lapply(temp, function(x) {
-  gsub("_vs.*","",
-       gsub("./data/SCANB/4_CN/raw/to_lennart//ascat.","",x))}))) %>% dplyr::rename(Ascat.id=1)
-# 1. convert epb IDs to normal sample IDs
-ascat.ids$Sample <- id.key$SENT.TUMOR[match(
-  ascat.ids$Ascat.id, id.key$SENT.TUMOR.aliquot)] 
-# 2. convert the other IDs to normal sample IDs
-ascat.ids$Sample2 <- id.key$SENT.TUMOR[match(
-  ascat.ids$Ascat.id, id.key$TUMOR.alias)] # 2. convert the other IDs to normal sample IDs
-ascat.ids$Sample <- ifelse(is.na(ascat.ids$Sample), ascat.ids$Sample2, ascat.ids$Sample)
-ascat.ids$Sample2 <- NULL
-
-# name the segments dataframes
-names(segment.files) <- ascat.ids$Sample
-
-
-
-# basis mut data # HERE GET TEH RIGHT FILE
+res$sampleID <- id.key$SENT.TUMOR[match(res$sample,id.key$SENT.TUMOR.aliquot)]
+res[is.na(res$sampleID),]$sampleID <- id.key$SENT.TUMOR[match(
+  res[is.na(res$sampleID),]$sample,id.key$TUMOR.alias)]
+res$sample <- NULL
+res$PAM50 <- rep("HER2E", length(sample))
+res$N_mut <- as.numeric(res$caveman_count) + as.numeric(res$pindel_count)
+res$caveman_count <- NULL
+res$pindel_count <- NULL
+#View(id.key)
+#View(res)
+scanb.muts <- res
+# basis mut data
 basis.muts <- loadRData("data/BASIS/1_clinical/raw/Summarized_Annotations_BASIS.RData") %>% 
   filter(ClinicalGroup == "ERposHER2neg" & PAM50_AIMS %in% c("LumA","LumB")) %>% 
   mutate(N_mut = nbrIndels + nbrSubs) %>% 
   dplyr::select(c("sample_name","PAM50_AIMS","N_mut")) %>% 
-  dplyr::rename(sample=sample_name,PAM50=PAM50_AIMS)
+  dplyr::rename(sampleID=sample_name,PAM50=PAM50_AIMS)
 
 # combined
 mutation.sample.counts <- rbind(basis.muts,scanb.muts)
@@ -153,12 +134,7 @@ p <- ggplot(mutation.sample.counts, aes(x=PAM50,y=N_mut,fill=PAM50)) +
   ylab("Mutational burden") +
   xlab("PAM50 subtype") +
   theme_bw() +
-  theme(axis.text.x = element_text(size = 60,margin = margin(t=10)),
-        axis.title.x = element_text(size = 60),
-        axis.text.y = element_text(size = 55,margin = margin(r=10)),
-        axis.title.y = element_text(size = 60),
-        plot.title = element_text(size=50),
-        legend.position = "none",
+  theme(legend.position = "none",
         panel.border = element_blank(), 
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
@@ -166,13 +142,14 @@ p <- ggplot(mutation.sample.counts, aes(x=PAM50,y=N_mut,fill=PAM50)) +
         axis.ticks = element_line(colour = "black", linewidth = 2),
         axis.ticks.length=unit(0.5, "cm")) +
   scale_fill_manual(values=setNames(c("#d334eb","#2176d5","#34c6eb"),
-                                    c("Her2e","LumA","LumB"))) +  
+                                    c("HER2E","LumA","LumB"))) +  
   scale_y_continuous(breaks = scales::breaks_pretty(10)) + 
-  ggtitle("TMB based on SBS and indels") +
+  ggtitle("TMB based on SBS and indels") 
   
 
-
+pdf(file = plot.file, onefile = TRUE)
 print(p)
+dev.off()
 plot.list <- append(plot.list,list(p))
 
 #######################################################################
