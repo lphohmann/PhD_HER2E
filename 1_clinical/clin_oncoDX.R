09ba46008eb255a9b1212cff3d7786e328002057
Lennart <l.p.hohmann@gmail.com>
2024-09-13 13:56:30 +0200

    added ROR score calc.

--- 1_clinical/clin_oncoDX.R
+++ 1_clinical/clin_oncoDX.R
@@ -12,7 +12,7 @@ cohort <- "SCANB"
 # packages
 source("scripts/4_CN/src/cn_functions.R")
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(genefu,biomaRt)
+pacman::p_load(genefu,biomaRt,readxl)
 #-------------------
 # set/create output directories
 # for plots
@@ -26,10 +26,10 @@ dir.create(data.path)
 infile.2 <- "./data/SCANB/1_clinical/raw/Summarized_SCAN_B_rel4_NPJbreastCancer_with_ExternalReview_Bosch_data.RData"
 infile.3 <- "./data/SCANB/2_transcriptomic/raw/genematrix_noNeg.Rdata"
 infile.4 <- "../Project_Basal/data/SCANB/2_transcriptomic/processed/ERp_LogScaled_gex.RData"
-
+infile.5 <- "../Project_Basal/data/SCANB/1_clinical/raw/SSP_SupplDataTable1.xlsx"
 # output paths
-plot.file <- paste0(output.path,cohort,"_oncoDX.pdf")
-txt.file <- paste0(output.path,cohort,"_oncoDX.txt")
+plot.file <- paste0(output.path,cohort,"_oncoDX&ROR.pdf")
+txt.file <- paste0(output.path,cohort,"_oncoDX&ROR.txt")
 #-------------------
 # storing objects 
 plot.list <- list() # object to store plots
@@ -82,6 +82,14 @@ gex <- as.data.frame(log2(gex + 1)) #log transform
 #gex.scaled[1:3,1:3]
 #gex[1:3,(ncol(gex)-3):ncol(gex)]
 
+# lod ssp ror data
+ror.dat <- read_excel(infile.5)
+ror.dat <- ror.dat[ror.dat$Follow.up.cohort == TRUE & 
+                     ror.dat$Sample %in% anno$Sample,
+                   c("Sample","NCN.PAM50","NCN.ROR.risk.cat",
+                     "NCN.ROR.binary.risk.cat",
+                     "NCN.ROR.asT0","NCN.ROR.asT1")]
+
 #######################################################################
 # calc risk scores
 #######################################################################
@@ -117,7 +125,7 @@ res <- oncotypedx(data=t.gex, annot=annot.dat,
                   do.scaling=TRUE)
 
 #######################################################################
-# summarize results: score
+# summarize results: score 
 #######################################################################
 dat <- res$score
 # subtype data
@@ -127,7 +135,6 @@ luma.dat <- as.numeric(as.vector(
   dat[anno$Sample[anno$NCN.PAM50 =="LumA"]]))
 lumb.dat <- as.numeric(as.vector(
   dat[anno$Sample[anno$NCN.PAM50 =="LumB"]]))
-
 
 # summary statistics
 her2.stats <- get_stats(her2.dat)
@@ -205,6 +212,78 @@ ot.df <- as.data.frame(cbind(variable,
 txt.out <- append(txt.out, c(capture.output(ot.df), "\n###########################################\n"))
 
 
+#######################################################################
+# PROSIGNA ROR SCORE CONTINUE HERE
+#######################################################################
+
+#######################################################################
+# summarize results: score 
+#######################################################################
+#dat <- ror.dat$NCN.ROR.asT0
+# subtype data
+her2.dat <- as.numeric(ror.dat[ror.dat$NCN.PAM50 =="Her2",]$NCN.ROR.asT0)
+luma.dat <- as.numeric(ror.dat[ror.dat$NCN.PAM50 =="LumA",]$NCN.ROR.asT0)
+lumb.dat <- as.numeric(ror.dat[ror.dat$NCN.PAM50 =="LumB",]$NCN.ROR.asT0)
+
+# summary statistics
+her2.stats <- get_stats(her2.dat)
+luma.stats <- get_stats(luma.dat)
+lumb.stats <- get_stats(lumb.dat)
+
+txt.out <- append(txt.out, 
+                  c("\n###########################################\n",
+                  "\n# ROR SCORE #\n",
+                  "\n###########################################\n"))
+
+txt.out <- append(txt.out, c(capture.output(luma.res), "\n###########################################\n"))
+txt.out <- append(txt.out, c("Her2e\n",capture.output(her2.stats), "\n",
+                             "LumA\n",capture.output(luma.stats), "\n",
+                             "LumB\n",capture.output(lumb.stats),
+                             "\n###########################################\n"))
+
+# mann whitney u tests
+luma.res <- wilcox.test(her2.dat, luma.dat)
+lumb.res <- wilcox.test(her2.dat, lumb.dat)
+
+txt.out <- append(txt.out, c(capture.output(luma.res), "\n###########################################\n"))
+txt.out <- append(txt.out, c(capture.output(lumb.res), "\n###########################################\n"))
+
+#######################################################################
+# summarize results: risk
+#######################################################################
+
+ct <- table(ror.dat$NCN.PAM50,ror.dat$NCN.ROR.risk.cat)
+
+res.luma <- fisher.test(ct[c("Her2","LumA"),])
+res.lumb <- fisher.test(ct[c("Her2","LumB"),])
+
+txt.out <- append(txt.out, 
+                  c("\nROR_riskgroup\n",
+                    "\n###########################################\n"))
+txt.out <- append(txt.out, c(capture.output(res.luma), "\n###########################################\n"))
+txt.out <- append(txt.out, c(capture.output(res.lumb), "\n###########################################\n"))
+
+variable <- c("high","intermediate","low")
+her.counts <- unname(ct["Her2",])
+luma.counts <- unname(ct["LumA",])
+lumb.counts <- unname(ct["LumB",])
+her.perc <- round((her.counts/sum(ct["Her2",]))*100,2)
+luma.perc <- round((luma.counts/sum(ct["LumA",]))*100,2)
+lumb.perc <- round((lumb.counts/sum(ct["LumB",]))*100,2)
+
+ot.df <- as.data.frame(cbind(variable,
+                             her.counts,her.perc,
+                             luma.counts,luma.perc,
+                             lumb.counts,lumb.perc))
+
+#names(ot.df) <- x
+
+#ot.df
+
+txt.out <- append(txt.out, c(capture.output(ot.df), "\n###########################################\n"))
+
+
+#######################################################################
 #######################################################################
 
 # save plots
